95071bdbe52fad947abac22cb37303312cadb011
Miika Pärni <miika.parni@helsinki.fi>
2017-11-14 22:29:26 +0200

    working chromedriver

--- koodi/viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ koodi/viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Tester {
 
     public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
